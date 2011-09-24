a6616f81a5c0bddd22c905538eec7b8ef02b46b1
hirak <hiraku@tojiru.net>
2011-09-24 21:55:50 +0900

    プラグインローダーのキャッシュを有効にしてみました。
    少しパフォーマンスが改善します。

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -5,4 +5,13 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     {
         require_once 'functions.php';
     }
+
+    protected function _initPluginCache()
+    {
+        $cachefile = APPLICATION_PATH . '/../data/cache/plugins.php';
+        if (file_exists($cachefile)) {
+            include_once $cachefile;
+        }
+        Zend_Loader_PluginLoader::setIncludeFileCache($cachefile);
+    }
 }
