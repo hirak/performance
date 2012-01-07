13655d793f0e5820b9e1e0e886b39fad912ab042
Hiraku NAKANO <hiraku@tojiru.net>
2012-01-07 23:47:08 +0900

    save()時にmodifiedを自動更新するようにした

--- application/models/Post.php
+++ application/models/Post.php
@@ -2,6 +2,12 @@
 
 class Model_Post extends Zend_Db_Table_Row_Abstract
 {
+    //modifiedを自動更新するよう、save()をオーバーライド
+    function save() {
+        $this->modified = date('c');
+        return parent::save();
+    }
+
     //モデル固有のメソッドを追加
     function sayHello() {
         echo 'Hello ' . __CLASS__;
